cf6edaa552a7a687c2868964f3897cf6d4199917
Mango932 <sammarchetti05@gmail.com>
2023-07-28 13:34:13 -0400

    started making the ui

---
+++ src/components/body.js
@@ -0,0 +1,13 @@
+import "./styles/body.css";
+import React from "react";
+
+function body() {
+    return (
+        <div className="container1">
+            <input></input>
+            <button placeholder="Input your text here">Check text</button>
+        </div>
+    );
+}
+
+export default body;
---
+++ src/components/navBar.js
@@ -0,0 +1,16 @@
+import "./styles/navBar.css";
+import React from "react";
+
+function NavBar() {
+    return (
+        <div className="container">
+            <text>Ai Text Detector</text>
+            <div>
+                <text className="subTitle">Code</text>
+                <text className="subTitle">How?</text>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
